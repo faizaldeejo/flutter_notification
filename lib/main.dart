3d8a888b412e49b3fc1854116e762ac72865a152
Syed Faizal <syedfaizals@metaplore.com>
2026-08-28 19:23:43 +0530

    setup the push notification with http service

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_notification/firebase_options.dart';
 import 'package:flutter_notification/push_notification_service.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await dotenv.load(fileName: '.env');
 
   final PushNotificationService pushNotificationService = PushNotificationService();
 
--- lib/push_notification_service.dart
+++ lib/push_notification_service.dart
@@ -1,5 +1,13 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:googleapis_auth/auth_io.dart';
+import 'package:googleapis_auth/googleapis_auth.dart';
+import 'package:http/http.dart' as http;
 
 class PushNotificationService {
 
@@ -18,6 +26,68 @@ class PushNotificationService {
       print('A new onMessageOpenedApp event was published!');
     });
 
-
   }
+
+  Future<AccessCredentials> _getAccessToken() async {
+    final serviceAccountPath = dotenv.env['PATH_TO_SECRET'];
+
+    String serviceAccountJson = await rootBundle.loadString(
+      serviceAccountPath!,
+    );
+
+    // log("json: $serviceAccountJson");
+    final serviceAccount = ServiceAccountCredentials.fromJson(
+      serviceAccountJson,
+    );
+
+    final scopes = ['https://www.googleapis.com/auth/firebase.messaging'];
+
+    final client = await clientViaServiceAccount(serviceAccount, scopes);
+    return client.credentials;
+  }
+
+  Future<bool> sendPushNotification({
+    required String deviceToken,
+    required String title,
+    required String body,
+    Map<String, dynamic>? data,
+  }) async {
+    if (deviceToken.isEmpty) return false;
+
+    final credentials = await _getAccessToken();
+    final accessToken = credentials.accessToken.data;
+    final projectId = dotenv.env['PROJECT_ID'];
+
+    log("accessToken: $dotenv.env['PROJECT_ID']");
+
+    final url = Uri.parse(
+      'https://fcm.googleapis.com/v1/projects/$projectId/messages:send',
+    );
+
+    final message = {
+      'message': {
+        'token': deviceToken,
+        'notification': {'title': title, 'body': body},
+        'data': data ?? {},
+      },
+    };
+
+    final response = await http.post(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $accessToken',
+      },
+      body: jsonEncode(message),
+    );
+
+    if (response.statusCode == 200) {
+      print('Notification sent successfully.');
+      return true;
+    } else {
+      print('Failed to send notification: ${response.body}');
+      return false;
+    }
+  }
+
 }
